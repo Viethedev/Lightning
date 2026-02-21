4cdf18689fd7118d1dcc7b14984bedadb7720c7b
Viethedev <ntheviet1204@gmail.com>
2026-02-21 22:25:24 +0700

    Error analysis

--- tests/lexer_test.cpp
+++ tests/lexer_test.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <cstdio>
 #include "lexer.hpp"
 
@@ -5,12 +6,12 @@ int main() {
     std::string src = "if lang = Spanish\n\n  print Hola, 1234.0\r\n else\n\r  print Hello, 1234";
     Lexer lexer = Lexer(src);
     std::vector<Token> tokens = lexer.tokenize();
-    for (Token tok : tokens) {
+    Token tok;
+    printf("Size: %d\n", static_cast<int>(tokens.size()));
+    for (size_t i = 0; i < tokens.size() - 3; ++i) { // 3 EOF?
+        tok = tokens[i];
         printf("Type: %d, lexeme: \"%s\"\n", tok.type, src.substr(tok.offset, tok.length).c_str());
     }
-    // Will raise: 
-    // terminate called after throwing an instance of 'std::out_of_range'
-    //   what():  basic_string::substr: __pos (which is 69) > this->size() (which is 67)
-    // because try to get EOF substring in source
+    printf("Type: EOF, lexeme:\n");
     return 0;
 }
